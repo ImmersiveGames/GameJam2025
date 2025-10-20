bf1b80a9eef89afa7b7a0b86b18d1ba1b349aa3d
Renato Innocenti <renato.innocenti@gmail.com>
2025-10-19 21:18:10 -0300

    Allow pooled bullets to propagate spawner actor when dealing damage

--- Assets/_ImmersiveGames/Scripts/DamageSystem/DamageDealer.cs
+++ Assets/_ImmersiveGames/Scripts/DamageSystem/DamageDealer.cs
@@ -1,5 +1,7 @@
-﻿using _ImmersiveGames.Scripts.ActorSystems;
+using _ImmersiveGames.Scripts.ActorSystems;
 using _ImmersiveGames.Scripts.ResourceSystems.Configs;
+using _ImmersiveGames.Scripts.Utils.DebugSystems;
+using _ImmersiveGames.Scripts.Utils.PoolSystems;
 using UnityEngine;
 
 namespace _ImmersiveGames.Scripts.DamageSystem
@@ -7,7 +9,6 @@ namespace _ImmersiveGames.Scripts.DamageSystem
     /// <summary>
     /// Causa dano em colisão com objetos que tenham DamageReceiver.
     /// </summary>
-    [RequireComponent(typeof(ActorMaster))]
     public class DamageDealer : MonoBehaviour, IDamageDealer
     {
         [Header("Configuração de Dano")]
@@ -18,9 +19,20 @@ namespace _ImmersiveGames.Scripts.DamageSystem
         [Header("Camadas válidas para causar dano")]
         [SerializeField] private LayerMask targetLayers;
 
-        private ActorMaster actor;
+        private IActor actor;
+        private PooledObject pooledObject;
 
-        private void Awake() => actor = GetComponent<ActorMaster>();
+        private void Awake()
+        {
+            actor = GetComponent<IActor>();
+            pooledObject = GetComponent<PooledObject>();
+
+            if (actor == null && pooledObject == null)
+            {
+                DebugUtility.LogWarning<DamageDealer>(
+                    "Nenhum IActor ou PooledObject encontrado. O dano ficará sem ator associado.", this);
+            }
+        }
 
         private void OnCollisionEnter(Collision collision)
         {
@@ -44,8 +56,9 @@ namespace _ImmersiveGames.Scripts.DamageSystem
             if (receiver == null) return;
 
             var contact = collision.contacts.Length > 0 ? collision.contacts[0] : default;
+            var sourceActor = ResolveActor();
             var ctx = new DamageContext(
-                actor.ActorId,
+                sourceActor?.ActorId,
                 receiver.GetReceiverId(),
                 baseDamage,
                 targetResource,
@@ -55,6 +68,17 @@ namespace _ImmersiveGames.Scripts.DamageSystem
             );
 
             DealDamage(receiver, ctx);
+        }
+
+        private IActor ResolveActor()
+        {
+            if (actor != null)
+                return actor;
+
+            if (pooledObject != null && pooledObject.Spawner != null)
+                return pooledObject.Spawner;
+
+            return null;
         }
 
         public void DealDamage(IDamageReceiver target, DamageContext ctx)
