c5a935c9c810a58f9dfa65fb6bb6db6d600efa45
Renato Innocenti <renato.innocenti@gmail.com>
2025-11-17 10:59:04 -0300

    Reset eater state timers on re-entry

--- Assets/_ImmersiveGames/Scripts/StateMachineSystems/StateMachine.cs
+++ Assets/_ImmersiveGames/Scripts/StateMachineSystems/StateMachine.cs
@@ -31,9 +31,6 @@ namespace _ImmersiveGames.Scripts.StateMachineSystems
         }
 
         private void ChangeState(IState state) {
-            if (_currentNode != null && state == _currentNode.State)
-                return;
-
             var nextNode = GetNodeOrThrow(state.GetType());
             var previousState = _currentNode?.State;
             var nextState = nextNode.State;
