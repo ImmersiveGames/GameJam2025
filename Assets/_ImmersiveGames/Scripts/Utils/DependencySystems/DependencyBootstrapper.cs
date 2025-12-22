4dfd99abf51aa6f5a54d420ede2a9aa623fe87dd
Renato Innocenti <renato.innocenti@gmail.com>
2025-12-22 13:09:20 -0300

    Revert legacy DependencyBootstrapper

--- Assets/_ImmersiveGames/Scripts/Utils/DependencySystems/DependencyBootstrapper.cs
+++ Assets/_ImmersiveGames/Scripts/Utils/DependencySystems/DependencyBootstrapper.cs
@@ -26,10 +26,6 @@ namespace _ImmersiveGames.Scripts.Utils.DependencySystems
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void Initialize()
         {
-#if NEWSCRIPTS_MODE
-            DebugUtility.Log(typeof(DependencyBootstrapper), "NEWSCRIPTS_MODE ativo: DependencyBootstrapper ignorado.");
-            return;
-#endif
             if (_initialized) return;
             _initialized = true;
 
@@ -185,14 +181,7 @@ namespace _ImmersiveGames.Scripts.Utils.DependencySystems
 
 #if UNITY_EDITOR
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
-        private static void ResetStatics()
-        {
-#if NEWSCRIPTS_MODE
-            DebugUtility.Log(typeof(DependencyBootstrapper), "NEWSCRIPTS_MODE ativo: ResetStatics ignorado.");
-            return;
-#endif
-            _initialized = false;
-        }
+        private static void ResetStatics() => _initialized = false;
 #endif
     }
 }
