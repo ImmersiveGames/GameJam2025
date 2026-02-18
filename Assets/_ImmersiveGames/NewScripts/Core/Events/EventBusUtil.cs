ab6f563a486c57786ed826062b3c307b58eca0dc
Renato Innocenti <renato.innocenti@gmail.com>
2026-02-18 15:38:19 -0300

    Fix: guard UnityEditor in EventBusUtil for player builds

--- Assets/_ImmersiveGames/NewScripts/Core/Events/EventBusUtil.cs
+++ Assets/_ImmersiveGames/NewScripts/Core/Events/EventBusUtil.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 
 namespace _ImmersiveGames.NewScripts.Core.Events
